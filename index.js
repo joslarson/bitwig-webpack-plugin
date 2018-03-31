be134770e5685ae08073cff5a380eaffd29160e8
Joseph Larson <larson.joseph@gmail.com>
2018-03-30 23:01:45 -0600

    finish webpack 4 support

--- index.js
+++ index.js
@@ -2,31 +2,37 @@ const ConcatSource = require('webpack-sources').ConcatSource;
 const sortChunks = require('html-webpack-plugin/lib/chunksorter.js').dependency;
 
 class BitwigWebpackPlugin {
-    optimizeChunkAssets(compilation) {
-        compilation.plugin('optimize-chunk-assets', (chunks, callback) => {
-            const orderedChunks = sortChunks(chunks, compilation.chunkGroups);
-            const files = orderedChunks.reduce((result, chunk) => [...result, ...chunk.files], []);
-            const entries = files.filter(file => file.match(/\.control\.js$/));
-            const commons = files.filter(file => !file.match(/\.control\.js$/));
+    optimizeChunkAssets(compilation, chunks) {
+        console.log(compilation.chunkGroups);
+        const orderedChunks = sortChunks(chunks, compilation.chunkGroups);
+        const files = orderedChunks.reduce((result, chunk) => [...result, ...chunk.files], []);
+        const entries = files.filter(file => file.match(/\.control\.js$/));
+        const commons = files.filter(file => !file.match(/\.control\.js$/));
 
-            entries.forEach(entryFile => {
-                const banner = `window = this;\n${commons
-                    .map(commonsFile => `load('${commonsFile}');`)
-                    .join('\n')}\n`;
-                compilation.assets[entryFile] = new ConcatSource(
-                    banner,
-                    compilation.assets[entryFile]
-                );
-            });
-            callback();
+        entries.forEach(entryFile => {
+            const banner = `window = this;\n${commons
+                .map(commonsFile => `load('${commonsFile}');`)
+                .join('\n')}\n`;
+            compilation.assets[entryFile] = new ConcatSource(banner, compilation.assets[entryFile]);
         });
     }
+
     apply(compiler) {
-        // setup hooks for webpack 4
         if (compiler.hooks) {
-            compiler.hooks.compilation.tap('BitwigWebpackPlugin', this.optimizeChunkAssets);
+            // setup hooks for webpack 4+
+            compiler.hooks.compilation.tap('BitwigWebpackPlugin', compilation =>
+                compilation.hooks.optimizeChunkAssets.tap('BitwigWebpackPlugin', chunks =>
+                    this.optimizeChunkAssets(compilation, chunks)
+                )
+            );
         } else {
-            compiler.plugin('compilation', this.optimizeChunkAssets);
+            // setup hooks for pre webpack 4
+            compiler.plugin('compilation', compilation =>
+                compilation.plugin('optimize-chunk-assets', (chunks, callback) => {
+                    this.optimizeChunkAssets(compilation, chunks);
+                    callback();
+                })
+            );
         }
     }
 }
